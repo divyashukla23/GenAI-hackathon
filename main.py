5e215227bbe0a036e2a7501f514d66711094ba39
Divya <divyashukla20993@gmail.com>
2023-08-02 13:33:11 +0530

    Refactor. Remove unnneccsary comments

--- main.py
+++ main.py
@@ -1,40 +1,7 @@
 # # Read the value of array : unmanaged and for every element use the logic : take id and type and generate terraform import command:
 # # Input: JSON(results.json)
 # # Output: terraform import resource_name.module_name id
-
-# import json
-# def generate_terraform_import_commands(json_file_path):
-#     try:
-#         with open(json_file_path, 'r') as file:
-#             data = json.load(file)
-
-#         if "unmanaged" in data:
-#             unmanaged_resources = data["unmanaged"]
-#             for idx, resource in enumerate(unmanaged_resources, start=1):
-#                 resource_id = resource.get("id")
-#                 resource_type = resource.get("type")
-#                 if resource_id and resource_type:
-#                     module_name = f"resource{idx}"
-#                     import_command = f"terraform import {resource_type}.{module_name} {resource_id}"
-#                     tf_block = f"resource \"{resource_type}\" \"{module_name}\"{{}}"
-#                     print(import_command)
-#                     print(tf_block)
-#                 else:
-#                     print(f"Skipping invalid resource: {resource}")
-
-#     except FileNotFoundError:
-#         print("Error: JSON file not found.")
-#     except json.JSONDecodeError:
-#         print("Error: Invalid JSON format.")
-
-# # Example usage:
-# json_file_path = "result.json"
-# generate_terraform_import_commands(json_file_path)
-
-
-# # to run: /opt/homebrew/bin/python3 /Users/divyashukla/Desktop/GenAI/main.py
 import json
-
 def generate_terraform_import_commands(json_file_path):
     try:
         with open(json_file_path, 'r') as file:
@@ -60,7 +27,6 @@ def generate_terraform_import_commands(json_file_path):
             with open("command.sh", "w") as bash_file:
                 for command in commands:
                     bash_file.write(command + "\n")
-
             # Print the commands to the console (optional)
             # for command in commands:
             #     print(command)
