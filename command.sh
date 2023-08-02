8933cb5c4c709844a83eec08afd231602c0ac185
Divya <divyashukla20993@gmail.com>
2023-08-02 13:26:32 +0530

    Add. Tf command outputs gets printed in a bash file rather than the terminal

---
+++ command.sh
@@ -0,0 +1,6 @@
+terraform import aws_ami.resource1 ami-036f08a3fadcb8dc1
+resource "aws_ami" "resource1"{}
+terraform import aws_ami.resource2 ami-04bbae7385a7f5b12
+resource "aws_ami" "resource2"{}
+terraform import aws_ami.resource3 ami-04bbae7385a7f5b12
+resource "aws_ami" "resource3"{}
--- main.py
+++ main.py
@@ -1,8 +1,40 @@
-# Read the value of array : unmanaged and for every element use the logic : take id and type and generate terraform import command:
-# Input: JSON(results.json)
-# Output: terraform import resource_name.module_name id
+# # Read the value of array : unmanaged and for every element use the logic : take id and type and generate terraform import command:
+# # Input: JSON(results.json)
+# # Output: terraform import resource_name.module_name id
 
+# import json
+# def generate_terraform_import_commands(json_file_path):
+#     try:
+#         with open(json_file_path, 'r') as file:
+#             data = json.load(file)
+
+#         if "unmanaged" in data:
+#             unmanaged_resources = data["unmanaged"]
+#             for idx, resource in enumerate(unmanaged_resources, start=1):
+#                 resource_id = resource.get("id")
+#                 resource_type = resource.get("type")
+#                 if resource_id and resource_type:
+#                     module_name = f"resource{idx}"
+#                     import_command = f"terraform import {resource_type}.{module_name} {resource_id}"
+#                     tf_block = f"resource \"{resource_type}\" \"{module_name}\"{{}}"
+#                     print(import_command)
+#                     print(tf_block)
+#                 else:
+#                     print(f"Skipping invalid resource: {resource}")
+
+#     except FileNotFoundError:
+#         print("Error: JSON file not found.")
+#     except json.JSONDecodeError:
+#         print("Error: Invalid JSON format.")
+
+# # Example usage:
+# json_file_path = "result.json"
+# generate_terraform_import_commands(json_file_path)
+
+
+# # to run: /opt/homebrew/bin/python3 /Users/divyashukla/Desktop/GenAI/main.py
 import json
+
 def generate_terraform_import_commands(json_file_path):
     try:
         with open(json_file_path, 'r') as file:
@@ -10,6 +42,8 @@ def generate_terraform_import_commands(json_file_path):
 
         if "unmanaged" in data:
             unmanaged_resources = data["unmanaged"]
+            commands = []  # To store the Terraform import commands
+
             for idx, resource in enumerate(unmanaged_resources, start=1):
                 resource_id = resource.get("id")
                 resource_type = resource.get("type")
@@ -17,10 +51,19 @@ def generate_terraform_import_commands(json_file_path):
                     module_name = f"resource{idx}"
                     import_command = f"terraform import {resource_type}.{module_name} {resource_id}"
                     tf_block = f"resource \"{resource_type}\" \"{module_name}\"{{}}"
-                    print(import_command)
-                    print(tf_block)
+                    commands.append(import_command)
+                    commands.append(tf_block)
                 else:
                     print(f"Skipping invalid resource: {resource}")
+
+            # Save the Terraform import commands to a bash script file
+            with open("command.sh", "w") as bash_file:
+                for command in commands:
+                    bash_file.write(command + "\n")
+
+            # Print the commands to the console (optional)
+            # for command in commands:
+            #     print(command)
 
     except FileNotFoundError:
         print("Error: JSON file not found.")
@@ -30,6 +73,3 @@ def generate_terraform_import_commands(json_file_path):
 # Example usage:
 json_file_path = "result.json"
 generate_terraform_import_commands(json_file_path)
-
-
-# to run: /opt/homebrew/bin/python3 /Users/divyashukla/Desktop/GenAI/main.py
