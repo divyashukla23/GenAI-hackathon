d2c30bc0108fcf12042c933a15ef2a56db498807
Poonam Shelke <punamshelke.98@gmail.com>
2023-08-20 14:22:05 +0530

    Add. basic scripts for assuming role
    This is a basic setup. It contains some manual changes
    1. Creation of User manually in aws console. This user has permission to assume only one IAM role.
    2. Creating IAM role which can be assumed by the newly created user.
    3. This IAM role is given Admin permissions as of now but needs to be changed in future.
    
    All this is needed because we want to assume the IAM role from local and do the operation without risking to expose actual access key ID and access key secret. It was only referred once when running aws configure. These creds are now used to assume the role and use the temporary creds to do all other operations.

---
+++ assume-role.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Assuming the role and capturing the JSON output
+output=$(aws sts assume-role --role-arn arn:aws:iam::462066733505:role/HackathonAdmin --role-session-name TerraformSession)
+
+# Extracting individual credentials from the output and setting them as environment variables
+export AWS_ACCESS_KEY_ID=$(echo $output | jq -r .Credentials.AccessKeyId)
+export AWS_SECRET_ACCESS_KEY=$(echo $output | jq -r .Credentials.SecretAccessKey)
+export AWS_SESSION_TOKEN=$(echo $output | jq -r .Credentials.SessionToken)
+
+echo "Temporary credentials set for session."
