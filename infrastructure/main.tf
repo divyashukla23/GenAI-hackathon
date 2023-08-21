338ce1326b66fa4862e54df1741bf184bae6a8c4
Poonam Shelke <punamshelke.98@gmail.com>
2023-08-21 13:39:56 +0530

    Add. drifted public access block

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,9 +1,16 @@
 resource "aws_s3_bucket" "state_bucket" {
   bucket = var.bucket_name
-
   tags = {
     name        = var.bucket_name
     environment = var.environment
     owner       = var.owner
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_access_block" {
+  bucket                  = aws_s3_bucket.state_bucket.bucket
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
